e5983839ded4b1ab04515884afa2f122afe774a0
Sayne Rojas <saynegrojas@yahoo.com>
2020-07-18 19:05:26 -0700

    mod user-create

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { IUser } from '../users/user';
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
@@ -9,7 +9,8 @@ import { Router } from '@angular/router';
 export class UserCreateComponent implements OnInit {
 
   constructor(private router: Router) { }
-
+  
+  // public userModel = new IUser;
   ngOnInit() {
   }
   onBack(): void {
